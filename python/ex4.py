23b704113c55cc3789179129a9c8708ecdbc4ba4
roger andersen <supercity@ROGER-LINUX.(none)>
2014-01-24 21:18:11 +0100

    trenger litt mere hjelp med denne

---
+++ python/ex4.py
@@ -0,0 +1,17 @@
+cars = 100 #say its 100 cars max available
+space_in_a_car = 4.0 # 
+drivers = 30 # there are 30 drivers 
+passengers = 90 #max 90 passengers total
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car #carpool need to be = card driven and spaces 
+average_passengers_per_car = passengers / cars_driven
+
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
+
