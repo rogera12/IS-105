e87e40d1800e355b4fd3ad8786e2960f36aef83d
roger andersen <supercity@ROGER-LINUX.(none)>
2014-01-28 22:11:31 +0100

    ex7

---
+++ python/ex7.py
@@ -0,0 +1,22 @@
+print "Mary had a little lamb." #printing out
+print "Its fleece was white as %s." % 'snow' # printing out
+print "And everywhere that Mary went." #printing out
+print "." * 10  # what'd that do? printing out 10 commas 
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, #printing out en1 to end 12 
+print end7 + end8 + end9 + end10 + end11 + end12
+
