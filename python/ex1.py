17519af42daf50a381524d63044780aea5e665ac
roger andersen <supercity@ROGER-LINUX.(none)>
2014-01-28 22:07:13 +0100

    ex1

--- python/ex1.py
+++ python/ex1.py
@@ -1,4 +1,4 @@
-print "Hello World"
+print "Roger" #uses to comments and if you write # in the beginning the line is a comment
 print "Hello Again"
 print "I like typing this."
 print "This is fun."
