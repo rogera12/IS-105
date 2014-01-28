b49ac9d6a6b28c96d4c414c85f15e1ba58a54f2b
roger andersen <supercity@ROGER-LINUX.(none)>
2014-01-28 22:08:44 +0100

    ex3

--- python/ex3.py
+++ python/ex3.py
@@ -1,23 +1,23 @@
-print "I will now count my chickens:"
+print "I will now count my chickens:" #printing out
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+print "Hens", 25 + 30 / 6  # +plus /dividing print out how many hens
+print "Roosters", 100 - 25 * 3 % 4 #print out how many roosters
 
-print "Now I will count the eggs:"
+print "Now I will count the eggs:" # print out the sentence
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #print out eggs
 
-print "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3 + 2 < 5 - 7?" #print out everything ""
 
-print 3 + 2 < 5 - 7
+print 3 + 2 < 5 - 7 #print out that its false
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print "What is 3 + 2?", 3 + 2 #print out sentence and sum
+print "What is 5 - 7?", 5 - 7 #print out sentence and sum
 
-print "Oh, that's why it's False."
+print "Oh, that's why it's False." #print out sentence
 
-print "How about some more."
+print "How about some more." #print out sentence
 
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print "Is it greater?", 5 > -2 #print out question and that its true that 5>-2
+print "Is it greater or equal?", 5 >= -2 #print out question and that 5>=-2 true
+print "Is it less or equal?", 5 <= -2 #print out question that 5<= -2 false
